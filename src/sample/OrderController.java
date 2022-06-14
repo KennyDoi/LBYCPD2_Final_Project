8d982e5b2cae72979b0345ac30d81f37c0cb2285
migellej <migelle_malinis@dlsu.edu.ph>
2022-06-14 23:37:32 +0800

    Order Controller GUI Started

--- src/sample/OrderController.java
+++ src/sample/OrderController.java
@@ -2,16 +2,48 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class OrderController {
+public class OrderController extends Main implements Initializable {
+
+    @FXML
+    public static DatePicker dp;
+
+
+    @FXML
+    public void getSelectedDate(MouseEvent event){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @FXML
     void goMenu(MouseEvent event) {
@@ -31,5 +63,10 @@ public class OrderController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
